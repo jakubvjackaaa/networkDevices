096fd22a014032b099e0deae66fcc2c76fa719f0
jakubvjackaaa <unknownuser33@email.cz>
2025-06-29 19:26:56 +0200

    test2

--- Database.cs
+++ Database.cs
@@ -26,51 +26,13 @@ namespace networkDevices
             {
                 Console.WriteLine("Device ID:" + input.ID + " doesn't exists.");
             }                
-        }
-        public void getKeys()
-        {
-            foreach (IDevice i in Data.Keys) { Console.WriteLine(i.ID +":"+i); }
-        }
+        }        
         public void getDevices()
         {
             foreach(KeyValuePair<IDevice,List<User>> item in Data)
             {
                 Console.WriteLine("[" + item.Key.ID + "]" + " : ("+item.Key.Hostname+";"+item.Key.MAC+") Users: "+string.Join(";",item.Value));
             }
-        }
-        public void getDataFromFile(string path)
-        {
-            try
-            {
-                FileStream fileStream = new FileStream(path,FileMode.Open);
-                using (StreamReader streamReader = new StreamReader(fileStream))
-                {
-                    while (!streamReader.EndOfStream)
-                    {
-                        IDevice device;
-                        string line = streamReader.ReadLine();
-                        string[] array = line.Split(',');
-                        if (array[2] == "R")
-                        {
-                            device = new Router(array[0], array[1]);
-                            Data.Add(device, new List<User>());
-                        }
-                        else if (array[2] == "S")
-                        {
-                            device = new Switch(array[0], array[1]);
-                            Data.Add(device, new List<User>());
-                        }                        
-                    }     
-                }
-            }
-            catch(FileNotFoundException e)
-            {
-                Console.WriteLine(e);
-            }
-            finally
-            {
-                Console.WriteLine("Process finished");
-            }
-        }        
+        }                
     }
 }
